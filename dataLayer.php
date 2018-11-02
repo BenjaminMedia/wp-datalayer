79e8dd78cfeffaed6ddac1a75624dfb5e548da5b
Michael Sørensen <michael.sorensen@bonnier.dk>
2018-11-02 09:38:03 +0100

    Version bump

--- dataLayer.php
+++ dataLayer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wp DataLayer
  * Plugin URI: http://bonnierpublications.com
  * Description: WordPress Datalayer implementation
- * Version: 0.1.10
+ * Version: 0.1.11
  * Author: Michael Sørensen
  * Author URI: http://bonnierpublications.com
  */
