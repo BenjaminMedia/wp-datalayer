44603a84bc623fb632cb67bb70e2d2bd4d17ecc0
Michael Sørensen <michael.sorensen@bonnier.dk>
2019-01-24 10:44:02 +0100

    Added text option for GTM code

--- src/Services/ScriptService.php
+++ src/Services/ScriptService.php
@@ -41,7 +41,7 @@ class ScriptService
             return;
         }
 
-        $gtmContainerId = getenv('GTM_CONTAINER_ID') ? getenv('GTM_CONTAINER_ID') : false;
+        $gtmContainerId = $this->getGtmCotainerId();
 
         if (!$gtmContainerId) {
             return;
@@ -66,7 +66,7 @@ j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
             return;
         }
 
-        $gtmContainerId = getenv('GTM_CONTAINER_ID') ? getenv('GTM_CONTAINER_ID') : false;
+        $gtmContainerId = $this->getGtmCotainerId();
 
         if (!$gtmContainerId) {
             return;
@@ -76,5 +76,32 @@ j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
         $gtm .= '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $gtmContainerId . '" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
         $gtm .= '<!-- End Google Tag Manager (noscript) -->';
         echo $gtm;
+    }
+
+    private function getGtmCotainerId()
+    {
+        if ($this->getEnvGtmContainerId() !== false) {
+            return $this->getEnvGtmContainerId();
+        }
+
+        if ($this->getDbGtmCotainerId() !== false) {
+            return $this->getDbGtmCotainerId();
+        }
+
+        return false;
+    }
+
+    private function getEnvGtmContainerId()
+    {
+        return getenv('GTM_CONTAINER_ID')
+            ? getenv('GTM_CONTAINER_ID')
+            : false;
+    }
+
+    private function getDbGtmCotainerId()
+    {
+        return !empty(BonnierDataLayer::instance()->getSettings()->get_setting_value('gtm_code'))
+            ? BonnierDataLayer::instance()->getSettings()->get_setting_value('gtm_code')
+            : false;
     }
 }
