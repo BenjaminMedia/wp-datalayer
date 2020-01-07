4c8865e36e96e963324c3d9ee87d607785f27089
Martin Shi <186@186.dk>
2020-01-07 13:56:18 +0100

    bumped version

--- dataLayer.php
+++ dataLayer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wp DataLayer
  * Plugin URI: http://bonnierpublications.com
  * Description: WordPress Datalayer implementation
- * Version: 0.1.19
+ * Version: 0.1.20
  * Author: Michael Sørensen
  * Author URI: http://bonnierpublications.com
  */
